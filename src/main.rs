97ae004f573745030da6c7054deb3559140825f2
Simon S <107688168+jit-compiler@users.noreply.github.com>
2024-11-19 13:39:00 +0100

    ?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod scan;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Getting hosts...");
+    let hosts = scan::scan_network(6969, 1000);
+
+    println!("Hosts: {:?}", hosts);
 }
