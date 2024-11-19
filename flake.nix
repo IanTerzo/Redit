bd552e739260d64b3068da61518c1fe7e830273f
NilsNachname <142907554+NilsNachname@users.noreply.github.com>
2024-11-19 12:48:07 +0100

    Add Nix support

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    buildInputs = [];
+    redit = (pkgs.rustPlatform.buildRustPackage {
+      name = "redit";
+      src = ./.;
+      cargoLock = {
+        lockFile = ./Cargo.lock;
+      };
+      nativeBuildInputs = with pkgs; [
+        rustPlatform.bindgenHook
+        pkg-config
+      ];
+      inherit buildInputs;
+    });
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        (final: prev: {
+          inherit redit;
+        })
+      ];
+    };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = buildInputs ++ (with pkgs; [
+        cargo
+        rustc
+      ]);
+    };
+    packages.x86_64-linux.default = redit;
+  };
+}
