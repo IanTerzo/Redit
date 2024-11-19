ee4aa7fabc739cf0f0347738dceb429a724613d2
Ian Baldelli <79420376+IanTerzo@users.noreply.github.com>
2024-11-19 12:48:01 +0100

    Upload modue and types module

---
+++ src/types.rs
@@ -0,0 +1,8 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct UploaderInfo {
+    pub public: bool,
+    pub name: String,
+    pub files_size: i32
+}
